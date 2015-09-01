ad683c516d1c6db8c14dea45fbbd661f630c3860
Jonathan Boiser <jboiser@gmail.com>
2015-08-31 21:23:36 -0500

    some fonts.

--- brewfile.sh
+++ brewfile.sh
@@ -54,6 +54,8 @@ brew install zsh
 # Fonts
 brew tap caskroom/fonts
 brew cask install font-source-code-pro
+brew cask install font-hasklig
+brew cask install font-fira-code
 
 brew cleanup
 brew cask cleanup
